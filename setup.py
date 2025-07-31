563c451d2d2051cdffbe2ab6c89a328a1079f5d0
georgiygusev <georgiy.gus@yandex.ru>
2025-07-31 23:22:38 +0300

    remove setup.py and added pyproject.toml

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="minio_pool_goginski",
-    version="0.1.2",
+    name="src",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         "aioboto3",
